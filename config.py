013c0202a677b03bc4c6b4709643a7641e47b6e6
pathcoder4408 <53309628+pathcoder4408@users.noreply.github.com>
2024-04-21 10:10:48 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Configuration settings for notifications and other services
+TWILIO_ACCOUNT_SID = 'YOUR_TWILIO_ACCOUNT_SID'
+TWILIO_AUTH_TOKEN = 'YOUR_TWILIO_AUTH_TOKEN'
+TWILIO_NUMBER = '+YourTwilioNumber'
+MAIL_FROM_ADDRESS = 'your-email@example.com'
